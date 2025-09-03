8d1148bc82000a9d963e094966bf98376d48274f
Jose Andres <andres.henao9@udea.edu.co>
2025-09-02 20:20:06 -0500

    Inicio de la interfaz gráfica Signed-off-by: Jose Andres <andres.henao9@udea.edu.co>

--- interfaz reconocimiento vial.py
+++ interfaz reconocimiento vial.py
@@ -13,6 +13,8 @@ raiz= Tk()
 MyFrame= Frame(raiz,width=500,height=400)
 MyFrame.pack()
 
+cuadroTexto= Entry(MyFrame)
+cuadroTexto.place(x=100,y=100)
 label=Label(MyFrame,text="Hola mundooooo",fg="blue")
 label.place(x=200,y=30)
 raiz.mainloop()
